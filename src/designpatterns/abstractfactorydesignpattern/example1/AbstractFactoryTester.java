1d27a7b4ed959dad2f60c98435b6c8668292c5c3
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2024-08-07 23:27:59 +0530

    separate package

--- src/designpatterns/abstractfactorydesignpattern/example1/AbstractFactoryTester.java
+++ src/designpatterns/abstractfactorydesignpattern/example1/AbstractFactoryTester.java
@@ -4,5 +4,6 @@ public class AbstractFactoryTester {
     public static void main(String[] as){
         CoffeeFactory coffeeFactory=new CoffeeFactory();
         System.out.println(coffeeFactory.getCoffee("Espresso"));
+        System.out.println(coffeeFactory.getCoffee("Robusta"));
     }
 }
